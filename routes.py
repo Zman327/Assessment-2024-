c3d0af48d1af81954fcebc82114fe14433494f1c
Zman327 <happes327@gmail.com>
2024-05-30 10:50:24 +1200

    Updated layout (Home page) and cleaned code

---
+++ routes.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+import sqlite3
+
+
+app = Flask(__name__)
+
+
+def do_sql(sql):
+    conn = sqlite3.connect('Fencing_hub.db')
+    cur = conn.cursor()
+    cur.execute(sql)
+    return cur.fetchall()
+
+
+@app.route('/events')
+def aboutpage():
+    x = do_sql("SELECT * FROM Male_Fencers")
+    return render_template('templates/events.html', male_fencers = x)
+
+if __name__ == "__main__":  # Last lines
+    app.run(debug=True)
+
+
